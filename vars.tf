9966887dc3c87eb4ae705688da9828c156e053d9
sirisha517 <sai.sirifriends8790@gmail.com>
2023-04-07 23:15:00 +0530

    rds

--- vars.tf
+++ vars.tf
@@ -2,7 +2,9 @@ variable "env" {}
 variable "engine" {}
 variable "engine_version" {}
 variable "instance_class" {}
-variable "db_name" {}
+variable "db_name" {
+  default = "dummy"
+}
 variable "backup_retention_period" {}
 variable "preferred_backup_window" {}
 variable "subnet_ids" {}
